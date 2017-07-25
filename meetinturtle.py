ea9fc445242a0f414f021e6012d1c279d2ff300d
wadi19-meet <wadi19@meet.mit.edu>
2017-07-25 18:31:51 +0300

    Stopping here

--- meetinturtle.py
+++ meetinturtle.py
@@ -1,5 +1,24 @@
 import turtle
 turtle.penup()
-turtle.pendown()
-turtle.goto(-200,-100)
 
+turtle.goto(-200,-100)
+turtle.pendown()
+
+turtle.goto(-200,100)
+
+turtle.goto(-150,-100)
+turtle.goto(-100,100)
+turtle.goto(-100,-100)
+turtle.penup()
+turtle.goto(-50,-100)
+turtle.pendown()
+turtle.goto(-50,100)
+turtle.goto(50,100)
+turtle.penup()
+turtle.goto(-50,0)
+turtle.pendown()
+turtle.goto(50,0)
+turtle.penup()
+turtle.goto(-50,-100)
+turtle.pendown()
+turtle.goto(50,-100)
